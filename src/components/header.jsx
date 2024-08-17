bbcbfd084875ef8c21edc68a2faaec3159ce8435
Gopal soni <Gopals.btech2022@iujaipur.edu.in>
2024-08-17 18:50:47 +0530

    Vieos works

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="relative h-screen overflow-hidden">
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        src="./public/bg22.mp4"
+       src="/bg22.mp4"
         autoPlay
         loop
         muted
