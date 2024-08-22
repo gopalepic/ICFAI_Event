a546bee3b13035771832fa7cc61cb4695be9871f
himanshu-guptaaa <himanshugupta00072@gmail.com>
2024-08-22 13:38:08 +0530

    epic

--- src/components/rules.jsx
+++ src/components/rules.jsx
@@ -5,18 +5,44 @@ const Rules = () => {
     <section className="p-8 bg-blue-200">
         <h2 className="text-2xl font-bold mb-4">Rules & Regulation</h2>
         < p
-        className="text-gray-700">1. All team members should be from the same college; no inter-college teams are allowed. However, members from different branches of the same college/institute are encouraged to form a team.
+        className="text-gray-700">1. All team members should be from the same college; no inter-college teams are allowed. However, members from different branches of the same college/institute are encouraged to form a team including Atleast one girl candidiate.
 <p>2.  Each team must mandatorily comprise 6 members, including...</p>
 
-<p>3. SPOC registration started in August 2024, and the last date for team nomination and idea submission by the College SPOC on the SIH portal is September 12th.</p>
+<p>3. </p>
 
-<p>4. For the Software Edition, after teams are shortlisted, each team will have the option of selecting 2 mentors from industry or academia. A mentor is expected to have relevant industry or academic experience to qualify. Each team of 6 members may or may not have 2 mentors.</p>
+<p>4. For the Software Edition, after teams are shortlisted, each team will have the option of selecting 2 mentors from industry or academia.</p>
 
 <p>5. If a team is selected for the final Hackathon, members will need to travel to the assigned nodal center, which can be anywhere in India.</p>
 
 <p>6. Shortlisted teams (6 students, including the team leader and 2 mentors) traveling for the grand finale will be eligible to receive reimbursement for the to-and-fro journey via 2nd Class sleeper railway fare.</p>
-<p>7.  The reimbursement amount will be paid at the grand finale nodal centers upon producing copies of train tickets and completing the relevant forms. The responsibility for booking train tickets lies with the teams themselves or their institutes.
-</p>
+<p>7.  The reimbursement amount will be paid at the grand finale nodal centers upon producing copies of train tickets and completing the relevant forms. The responsibility for booking train tickets lies with the teams themselves or their institutes.</p>
+
+
+<h1 className="text-2xl font-bold mb-4"> 
+    <b>PROBLEM STATEMENTS AND PRIZES TO BE WON</b></h1>
+<p>1.SIH2024 brings you problem statements from leading public and private organizations, union
+ministries, state governments and NGOs in India.</p>
+<p>2.There would be ONE single winning team for every problem statement posted on the portal. The
+prize money will be given by the collaborating ministry/ industry ONLY IF that organization likes
+the idea of the winning team. However, it is NOT mandatory to announce only one winning team
+against each problem statement. If the collaborating ministry/ industry conclude that none of the
+ideas are up-to-the-mark, they may not announce any win</p>
+<p>3. Amount of prize money to be awarded to each winning statement</p>
+<p>Teams are also encouraged to think out-of-the-box and provide creative solutions to themes
+under ‘Students Innovation’ category. Please note that only 100 teams’ max (Including theme
+based/Student Innovation) are allowed from one college</p>
+<p> Prize money for student innovation category will be Rs. 100,000/- per theme.</p>
+<button className="mt-4 sm:mt-8 bg-blue-600 text-white px-4 sm:px-6 py-2 sm:py-3 text-lg rounded-full hover:bg-black">
+  <a
+    href="https://www.sih.gov.in/sih2024PS?technology_bucket=QWxs&category=U29mdHdhcmU=&organization=QWxs&organization_type=QWxs"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Problem Statement
+  </a>
+</button>
+
+
 </p>
 
 
